9a9e4e4455e48f7f6f3eca8fbe895364bd53ceb0
Jhonathan Araujo <JhonathanAraujoBR@Hotmail.com>
2020-05-28 15:48:55 -0300

    Convertendo PDF para texto

--- GerandoPDF/Program.cs
+++ GerandoPDF/Program.cs
@@ -1,11 +1,13 @@
 ﻿
 using System;
-using System.Diagnostics;
+using System.Text;
 using System.IO;
+using System.Diagnostics;
 
 // Biblioteca iTextSharp com suas extensões
 using iTextSharp.text;
 using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
 
 namespace GerandoPDF
 {
@@ -13,17 +15,16 @@ namespace GerandoPDF
     {
         static void Main(string[] args)
         {
-            // Criando o coumento com de folha usada
+            // Caminho para o arquivo .pdf 
+            string path = @"C:\teste.pdf";
+
+            // Criando o doumento com tamanho de folha
             using (Document doc = new Document(PageSize.A4))
             {
-
                 // Setando margens na sequência: esquerdo, em cima, direito e em baixo
                 doc.SetMargins(40, 40, 40, 80);
                 // Adicionando as configuracoes
                 doc.AddCreationDate();
-
-                // Caminho para criação do aqrquivo .pdf 
-                string path = @"C:\teste.pdf";
 
                 // Criando o arquivo PDF em branco
                 PdfWriter writer = PdfWriter.GetInstance(doc, new FileStream(path, FileMode.Create));
@@ -32,10 +33,10 @@ namespace GerandoPDF
 
                 Paragraph paragraph = new Paragraph(String.Empty, new Font(Font.NORMAL, 14));
                 paragraph.Alignment = Element.ALIGN_JUSTIFIED;
-                paragraph.Add("=== TESTE === \n");
+                paragraph.Add("TESTE\n");
 
                 paragraph.Font = new Font(Font.NORMAL, 14, (int)System.Drawing.FontStyle.Bold);
-                paragraph.Add("=== TESTE negrito === \n");
+                paragraph.Add("TESTE negrito\n");
 
                 Paragraph loremIpsum = new Paragraph(String.Empty, new Font(Font.NORMAL, 14));
 
@@ -46,9 +47,25 @@ namespace GerandoPDF
                 doc.Add(paragraph);
                 doc.Add(loremIpsum);
 
+                Console.WriteLine(" === PDF GERADO COM SUCESSO === ");
+
                 //Abrindo o arquivo após cria-lo
                 Process.Start(path);
             }
+
+            // Convertendo PDF para texto
+            using (PdfReader reader = new PdfReader(path))
+            {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    sb.Append(PdfTextExtractor.GetTextFromPage(reader, i));
+                }
+
+                Console.WriteLine("\n\n === CONTEÚDO DO ARQUIVO === \n");
+                Console.WriteLine(sb.ToString());
+            }
+            Console.ReadLine();
         }
     }
 }
